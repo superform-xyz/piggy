fc14b7f8ac61e317a554fd325db0b1393cd78558
0xTimepunk <jldcmartins@gmail.com>
2024-11-20 12:08:31 +0000

    fix: verification

--- script/run/deploy_piggy.sh
+++ script/run/deploy_piggy.sh
@@ -4,7 +4,8 @@
 export BASE_SEPOLIA_RPC_URL=$(op read op://5ylebqljbh3x6zomdxi3qd7tsa/BASE_SEPOLIA_RPC_URL/credential)
 export BASE_SEPOLIA_ETHERSCAN_API_KEY=$(op read op://5ylebqljbh3x6zomdxi3qd7tsa/BASESCAN_API_KEY/credential)
 export BASE_SEPOLIA_VERIFIER_URL=https://api.tenderly.co/api/v1/account/superform/project/v1/etherscan/verify/network/84532/public
- 
+export TENDERLY_ACCESS_KEY=$(op read op://5ylebqljbh3x6zomdxi3qd7tsa/TENDERLY_ACCESS_KEY/credential)
+
 # Run the script
 
 echo Deploy Piggy on BaseSepolia: ...
@@ -12,7 +13,7 @@ forge script script/DeployPiggy.s.sol:DeployPiggy --sig "deploy()" \
     --rpc-url $BASE_SEPOLIA_RPC_URL \
     --account testDeployer \
     --sender 0x76e9b0063546d97a9c2fdbc9682c5fa347b253ba \
-    --etherscan-api-key $BASE_SEPOLIA_ETHERSCAN_API_KEY \
+    --etherscan-api-key $TENDERLY_ACCESS_KEY \
     --verify \
     --verifier-url $BASE_SEPOLIA_VERIFIER_URL \
     --broadcast \
